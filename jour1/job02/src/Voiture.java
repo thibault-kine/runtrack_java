086d66f8958c9604ab68074aa78f3a389d352bec
thibault-kine <thibault.kine@laplateforme.io>
2023-09-06 11:33:02 +0200

    job02 corrected

--- jour1/job02/src/Voiture.java
+++ jour1/job02/src/Voiture.java
@@ -1,25 +1,19 @@
 public class Voiture {
 
-    static int speed = 0;
+    int speed = 0;
 
-    public static void main(String[] args) {
-        start();
-        speedUp();
-        speedUp();
-        brake();
-    }
-
-    public static void start() {
+    public void start() {
         System.out.println("La voiture démarre.");
     }
 
-    public static void speedUp() {
+    public void speedUp() {
         speed += 10;
         System.out.printf("La vitesse est maintenant %dkm/h.\n", speed);
     }
 
-    public static void brake() {
+    public void brake() {
         speed = 0;
         System.out.printf("La voiture s'arrête. Vitesse réinitialisée à %dkm/h.\n", speed);
     }
 }
+
---
+++ jour1/job02/src/VoitureTest.java
@@ -0,0 +1,11 @@
+public class VoitureTest {
+
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture();
+
+        voiture.start();
+        voiture.speedUp();
+        voiture.speedUp();
+        voiture.brake();
+    }
+}
