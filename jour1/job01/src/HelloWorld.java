c8c245edc31954f703bd3fdf21e27e04d551dac0
thibault-kine <thibault.kine@laplateforme.io>
2023-09-06 10:44:23 +0200

    job01

---
+++ jour1/job01/src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // a Scanner is a way to read from external sources (input, files...)
+        String name = "";
+
+        System.out.print("java HelloWorld");
+        System.out.print("Veuillez saisir votre nom: ");
+        name = input.nextLine(); // reads the next line
+        System.out.printf("Hello %s", name);
+    }
+}
