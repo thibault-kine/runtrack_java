ef48005db2876c4053a3c87f06340ce2fcdabdd4
thibault-kine <thibault.kine@laplateforme.io>
2023-09-08 10:20:07 +0200

    job01 pushed

--- jour3/job01/src/ListeEntiersExercice.java
+++ jour3/job01/src/ListeEntiersExercice.java
@@ -3,13 +3,17 @@ import java.util.ArrayList;
 public class ListeEntiersExercice {
     public static void main(String[] args) {
 
+        // create a new list of Integers
         ArrayList<Integer> liste = new ArrayList<>();
+        // for each argument in args
         for(String arg : args) {
+            // cast the arg to an int and add it to the list
             liste.add(Integer.parseInt(arg));
         }
 
         int evenSum = 0;
         for(int n : liste) {
+            // if the number is even, add it to the total sum
             if(n % 2 == 0) evenSum += n;
         }
 
