e90755e585af7b43bc0b8ab4ae3da2e91fcc21a7
thibault-kine <91733294+thibault-kine@users.noreply.github.com>
2023-09-11 14:40:08 +0200

    Update CallableFutureExercice.java

--- jour4/job04/src/CallableFutureExercice.java
+++ jour4/job04/src/CallableFutureExercice.java
@@ -4,19 +4,24 @@ public class CallableFutureExercice {
     public static void main(String[] args) {
 
         Integer result;
+        // Creates a single-threaded executor (a "box" of threads)
         ExecutorService executor = Executors.newSingleThreadExecutor();
+        // Creates a promise of sending the result ("async" statement)
         Future<Integer> r = executor.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
+                // Just basic math
                 int a = Integer.parseInt(args[0]);
                 int b = Integer.parseInt(args[1]);
                 return a * b;
             }
         });
 
+        // Tells the executor that we don't need him anymore
         executor.shutdown();
 
         try {
+            // Basically an "await" statement
             result = r.get();
             System.out.printf("Résultat de la multiplication: %d", result);
         }
