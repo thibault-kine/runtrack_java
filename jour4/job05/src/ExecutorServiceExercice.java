e8b0b3d602999f4927da85bad6af23ccf59ace59
thibault-kine <thibault.kine@laplateforme.io>
2023-09-11 23:54:10 +0200

    job05 pushed

---
+++ jour4/job05/src/ExecutorServiceExercice.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.*;
+
+public class ExecutorServiceExercice {
+    public static void main(String[] args) {
+
+        int factor = 3;
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        for(int i = 1; i <= 3; i++) {
+            int x = i;
+            executor.submit(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    int prod = x * factor;
+                    System.out.printf("Résultat de la tâche %d: %d\n", x, prod);
+                    return null;
+                }
+            });
+        }
+
+        executor.shutdown();
+    }
+}
