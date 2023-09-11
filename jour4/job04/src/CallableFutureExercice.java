d4ab29f1456ad0b47eedea51fe88bc8b99d5b884
thibault-kine <thibault.kine@laplateforme.io>
2023-09-11 23:08:19 +0200

    job04 edited

--- jour4/job04/src/CallableFutureExercice.java
+++ jour4/job04/src/CallableFutureExercice.java
@@ -3,16 +3,19 @@ import java.util.concurrent.*;
 public class CallableFutureExercice {
     public static void main(String[] args) {
 
-        Integer result;
+        int result;
         // Creates a single-threaded executor (a "box" of threads)
         ExecutorService executor = Executors.newSingleThreadExecutor();
         // Creates a promise of sending the result ("async" statement)
+        // A Callable is an async function
         Future<Integer> future = executor.submit(new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
                 // Just basic math
-                int a = Integer.parseInt(args[0]);
-                int b = Integer.parseInt(args[1]);
+//                int a = Integer.parseInt(args[0]);
+                int a = 5;
+//                int b = Integer.parseInt(args[1]);
+                int b = 6;
                 return a * b;
             }
         });
